0edd6f82e26a14944758cbca0ba9c853451c0c3e
nohssam <nojm73@naver.com>
2024-11-25 12:19:04 +0900

    DB전 내용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './component/Header';
+import DayList from './component/DayList';
+import Day from './component/Day';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        {/* 라우터의 영향을 받지 않고 고정된 컴포넌트 */}
+        <Header />
+        <Routes>
+          {/* path='/' 는 첫페이지을 의미한다.(홈페이지) */}
+          <Route path='/' element={<DayList />} />
+
+          {/* 동적라우딩 */}
+          <Route path='/day/:day' element={<Day />} />
+
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/component/Day.jsx
@@ -0,0 +1,23 @@
+import { useParams } from "react-router-dom";
+import dummy from "../db/data.json"
+import Word from "./Word";
+
+function Day(props) {
+    //const day = 1;
+
+    // url에 포한된 day값을 가져오기 위해서 useParams()
+    const day = useParams().day;
+    const wordList = dummy.words.filter(k => (k.day) === Number(day))
+    return (
+        <table>
+            <tbody>
+                {/* {dummy.words.map(k => { */}
+                {wordList.map(k => {
+                    return <Word key={k.id} word={k} />
+                })}
+            </tbody>
+        </table>
+    );
+}
+
+export default Day;
